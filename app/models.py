d0d3258a41f6bce56d38f847b638c8e2843e9337
TheDangerousAsian <allentran561@gmail.com>
2022-04-28 21:03:11 -0700

    Combined

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,3 +11,9 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username} {self.email}>'
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        print(self.password_hash)
+   
+    def verify_password(self, pwd):
+       return check_password_hash(self.password_hash, pwd)
